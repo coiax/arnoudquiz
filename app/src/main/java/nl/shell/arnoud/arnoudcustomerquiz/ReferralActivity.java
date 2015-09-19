3d2f3830d9ae9e35ac3783ca0eeead0bb688e7d5
Jack Edge <yellowbounder@gmail.com>
2015-09-19 16:23:48 +0100

    Referral activity

--- app/src/main/java/nl/shell/arnoud/arnoudcustomerquiz/ReferralActivity.java
+++ app/src/main/java/nl/shell/arnoud/arnoudcustomerquiz/ReferralActivity.java
@@ -1,24 +1,22 @@
 package nl.shell.arnoud.arnoudcustomerquiz;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class EndActivity extends AppCompatActivity {
+public class ReferralActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_end);
+        setContentView(R.layout.activity_referral);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_end, menu);
+        getMenuInflater().inflate(R.menu.menu_referral, menu);
         return true;
     }
 
@@ -35,9 +33,5 @@ public class EndActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    public void continueButton(View view) {
-        Intent intent = new Intent(this, ReferralActivity.class);
-        startActivity(intent);
     }
 }
